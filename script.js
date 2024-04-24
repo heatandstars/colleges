6ae498a4107f02d33515352bf92bc6d717f2deb2
heatandstars <167944532+heatandstars@users.noreply.github.com>
2024-04-24 09:29:55 -0400

    bigger set us view

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ markList = [
   }
 ]
   
-const map = L.map('map').setView([37.0902, -95.7129], 5);
+const map = L.map('map').setView([39.879106446065634, -99.181777350521], 5);
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
